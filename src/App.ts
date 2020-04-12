327f9ac9fd53930125aa74d5cd451f4b9ff55892
Nijat Mahmudov <n.e.mahmudov@gmail.com>
2020-04-12 18:04:51 +0400

    feat: Show top 5 languages

--- src/App.ts
+++ src/App.ts
@@ -1,5 +1,5 @@
 import { LangChart } from './LangChart'
-import { LangStatsProvider } from './LangStatsProvider'
+import { LangStatsProvider, LangStats } from './LangStatsProvider'
 
 const w = window.innerWidth
 const h = window.innerHeight
@@ -10,13 +10,20 @@ const ctx = radarChart.getContext("2d")
 radarChart.setAttribute("width", w.toString())
 radarChart.setAttribute("height", h.toString())
 
+const top5 = (data: LangStats): LangStats => {
+    const buf: LangStats = {}
+    const arr = Object.keys(data).map( l => { return { l, s: data[l] } }).sort((a,b) => a.s + b.s)
+    for(let i=0; i<5; i++) buf[arr[i].l] = arr[i].s
+    return buf
+}
+
 window.addEventListener("load", () => {
     const token = prompt('Enter your auth token')
     new LangStatsProvider(token)
         .fetchData()
         .then(data => {
             new LangChart(ctx)
-                .setData(data)
+                .setData(top5(data))
 
             document.body.appendChild(radarChart)
         })
