ca32750c4293afb4a241d81387ac49854bb4348d
Nijat Mahmudov <n.e.mahmudov@gmail.com>
2020-04-12 16:47:23 +0400

    feat: Show chart based on user's AuthToken

--- src/App.ts
+++ src/App.ts
@@ -1,4 +1,5 @@
-import { Chart } from 'chart.js'
+import { LangChart } from './LangChart'
+import { LangStatsProvider } from './LangStatsProvider'
 
 const w = window.innerWidth
 const h = window.innerHeight
@@ -6,34 +7,19 @@ const h = window.innerHeight
 const radarChart: HTMLCanvasElement = document.createElement("canvas")
 const ctx = radarChart.getContext("2d")
 
-const rawLangData = {
-  "Go": 2018,
-  "Makefile": 603,
-  "Python": 158,
-  "C": 145,
-  "V": 51
-}
-
-const langData = { 
-    labels: Object.keys(rawLangData), 
-    datasets: [
-        { 
-            label: "Languages",
-            backgroundColor: "rgba(255,0,0,0.4)",
-            borderColor: "rgba(255,0,0,0.4)",
-            data: Object.keys(rawLangData).map( v => rawLangData[v] ) 
-        }
-    ]}
-
 radarChart.setAttribute("width", w.toString())
 radarChart.setAttribute("height", h.toString())
 
-new Chart(ctx, {
-    type: 'radar',
-    data: langData,
-    options: {}
-});
-
 window.addEventListener("load", () => {
-    document.body.appendChild(radarChart)
+    const token = prompt('Enter your auth token')
+    new LangStatsProvider(token)
+        .fetchData()
+        .then(data => {
+            new LangChart(ctx)
+                .setData(data)
+
+            document.body.appendChild(radarChart)
+        })
+        .catch(err => console.error(`could not fetch data: ${err}`))
+
 })
